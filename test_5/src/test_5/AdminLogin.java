c148f11f85f91d27ff2b80f177187f89d181b63c
chathudu <it20253912@my.sliit.lk>
2021-09-13 22:28:12 +0530

    second

---
+++ test_5/src/test_5/AdminLogin.java
@@ -0,0 +1,15 @@
+package test_5;
+
+public class AdminLogin {
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLocationRelativeTo(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
